5aa8fa2ea9cbb5a22ce636a22d8b186042192205
jekovniki <jekovniki95@gmail.com>
2025-02-02 11:24:59 +0200

    feat(role-permissions): add role and permissions modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { RoleModule } from './modules/role/role.module';
+import { PermissionModule } from './modules/permission/permission.module';
 
 @Module({
   imports: [
@@ -10,6 +13,9 @@ import { SharedModule } from './shared/shared.module';
       isGlobal: true,
     }),
     SharedModule,
+    AuthModule,
+    RoleModule,
+    PermissionModule,
   ],
   controllers: [AppController],
   providers: [AppService],

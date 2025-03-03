2eab04d1acb8c9059dc35587d1560424627f6c17
jekovniki <jekovniki95@gmail.com>
2025-03-03 11:02:22 +0200

    feat(entity): add entity endpoint

--- src/modules/entity/entity.module.ts
+++ src/modules/entity/entity.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EntityService } from './entity.service';
 import { EntityController } from './entity.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Entity } from './entities/entity.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Entity])],
   controllers: [EntityController],
   providers: [EntityService],
 })

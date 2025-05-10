a8e0c4cae0c9b2a61dc894c297bfa6c802a91f6c
jekovniki <jekovniki95@gmail.com>
2025-05-10 16:35:40 +0300

    feat(hardcode): hardcode the value

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',').map((origin) => origin.trim()) : ['http://localhost:5173'],
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? ['https://amc-ui-production.up.railway.app', 'https://amc-ui-production.up.railway.app/']
+        : true,
     credentials: true,
   }); // remove later
   await app.listen(3000);

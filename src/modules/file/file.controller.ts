f5faaeba0194b0a8d1b94d33fc029e512fd44adc
jekovniki <jekovniki95@gmail.com>
2025-06-17 09:06:59 +0300

    feat(public): make the methods public

--- src/modules/file/file.controller.ts
+++ src/modules/file/file.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { FileService } from './file.service';
 import { GetPublicUrlDto } from './dto/get-public-url.dto';
 import { GenerateUploadUrlDto } from './dto/generate-upload-url.dto';
+import { Public } from 'src/shared/decorator/public.decorator';
 
 @Controller({
   path: 'file',
@@ -10,11 +11,13 @@ import { GenerateUploadUrlDto } from './dto/generate-upload-url.dto';
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
+  @Public()
   @Post()
   async generateUploadUrl(@Body() dto: GenerateUploadUrlDto) {
     return await this.fileService.generatePresignedUploadUrl(dto.fileName, dto.contentType, dto.folder);
   }
 
+  @Public()
   @Get(':fileName')
   getPublicUrl(@Param() params: GetPublicUrlDto) {
     return this.fileService.getPublicUrl(params.fileName);
